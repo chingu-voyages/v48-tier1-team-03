532516a504cd5ec44cf8ff626c6617d584631b9f
Bastien Winant <bastien.winant@gmail.com>
2024-04-08 22:46:09 +0200

    refactor js code using array methods

--- script.js
+++ script.js
@@ -1,129 +1,63 @@
-// Get the container
-const cardContainer = document.getElementById("dinoCards");
+import data from './data.json' with { type: 'json' };
 
-// Get the search field
-const searchInput = document.getElementById("search");
+// DOM ELEMENTS
+const gridContainer = document.getElementById("grid-container");
+const gridSearchInput = document.getElementById("dinoName");
 
-// Function to create card element for each dino entry
-function entryCard(dino) {
-  // Create the container element
-  const containerEl = document.createElement("div");
-  containerEl.classList.add("col-lg-4", "col-md-6", "col-sm-12");
+const gridModal = document.getElementById("dinoModal");
+const modalImg = document.getElementById("modal-img");
+const modalTitle = document.getElementById("modal-title");
+const modalSubtitle = document.getElementById("modal-subtitle");
 
-  // Create the card element
-  const cardEl = document.createElement("div");
-  cardEl.classList.add("card", "px-lg-2");
-  containerEl.appendChild(cardEl);
-
-  // Create the image element
-  const image = document.createElement("img");
-  image.classList.add("card-img-top");
-  image.setAttribute("src", dino.imageSrc);
-  image.setAttribute("alt", dino.name);
-  image.setAttribute("data-bs-toggle", "modal");
-  image.setAttribute("data-bs-target", "#dinoModal");
-  image.setAttribute("data-id", dino.id);
-  cardEl.appendChild(image);
-
-  // Create the card body element
-  const cardBody = document.createElement("div");
-  cardBody.classList.add("card-body");
-  cardEl.appendChild(cardBody);
-
-  // Create the card title element
-  const cardTitle = document.createElement("h5");
-  cardTitle.classList.add("card-title");
-  cardTitle.textContent = dino.name;
-  cardTitle.setAttribute("data-bs-toggle", "modal");
-  cardTitle.setAttribute("data-bs-target", "#dinoModal");
-  cardTitle.setAttribute("data-id", dino.id);
-  cardBody.appendChild(cardTitle);
-
-  return containerEl;
+// HELPER FUNCTIONS
+function getDataEntry(entryID) {
+  for (const entry of data) {
+    if (entry.id == entryID) {
+      return entry;
+    }
+  }
 }
 
-
 // Data
-// Fetch the JSON dat
-function displayCards(substr){
-fetch('data.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data); // Use the JSON data here\
-      // Remove the current cards from the container
-        cardContainer.innerHTML = "";
-
-        // Loop through the data
-        data.forEach((dino) => {
-          if (dino.name.includes(substr)) {
-          // Create the card element using the entryCard function
-          const card = entryCard(dino);
-        
-          // Append the card to the container
-          cardContainer.appendChild(card);
-          }
-        });
-
-
-            // Further processing of the data here
-            // Add event listener for modal
-        const modal = document.getElementById("dinoModal");
-        
-        modal.addEventListener("show.bs.modal", function (event) {
-          // Button
-          const button = event.relatedTarget;
-          // Extract data-id 
-          const dinoId = button.getAttribute("data-id");
-          // Find the dinosaur object
-          const selectedDino = data.find((dino) => dino.id.toString() === dinoId);
-          
-          // Update modal content with the dinosaur's details
-          const modalTitle = modal.querySelector(".modal-title");
-          const modalImage = modal.querySelector(".dino-img");
-          const modalName = modal.querySelector(".dino-name");
-          const modalType = modal.querySelector(".dino-type");
-          const modalLength = modal.querySelector(".dino-length");
-          const modalDiet = modal.querySelector(".dino-diet");
-          const modalWhenLived = modal.querySelector(".dino-when-lived");
-          const modalTypeSpecies = modal.querySelector(".dino-type-species");
-          const modalDescription = modal.querySelector(".dino-description");
-        
-          modalTitle.textContent = "Dinosaur Details";
-          modalImage.src = selectedDino.imageSrc;
-
-          modalName.textContent = selectedDino.name;
-          modalType.textContent = `Type: ${selectedDino.typeOfDinosaur}`;
-          modalLength.textContent = `Length: ${selectedDino.length} meters`;
-          modalDiet.textContent = `Diet: ${selectedDino.diet}`;
-          modalWhenLived.textContent = `When Lived: ${selectedDino.whenLived}`;
-          modalTypeSpecies.textContent = `Type Species: ${selectedDino.typeSpecies}`;
-          modalDescription.textContent = `Description: ${selectedDino.description}`;
-        });
-  })
-  .catch(error => {
-    console.error('Error fetching JSON:', error);
-  });
-
+// Fetch the JSON data
+function renderCards(searchStr) {
+  fetch('./data.json')
+    .then(response => response.json())
+    .then(data => {
+      gridContainer.innerHTML = data.filter((entry) => entry.name.includes(searchStr))
+          .map((entry) =>
+            `<div class="col-12 col-sm-6 col-lg-4 col-xl-3">
+              <div class="card text-bg-dark">
+                <img src="${entry.imageSrc}" class="card-img opacity-25" alt="...">
+                <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center">
+                  <h5 class="card-title display-6 fs-3">${entry.name}</h5>
+                  <!--<p class="card-text">${entry.description}</p> -->
+                  <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dinoModal" data-id=${entry.id}>Learn More</a>
+                </div>
+              </div>
+            </div>`)
+          .join("\n");
+    })
+    .catch(error => {
+      console.error('Error fetching JSON:', error);
+    });
 }
 
 // EVENT LISTENERS
-search.addEventListener("input", () => {
-  // navigate to the page grid
-  cardContainer.scrollIntoView({
-    behavior: "smooth",
-    block: "start",
-    inline: "nearest"
-  });
+gridSearchInput.addEventListener("input", () => {
+  const searchValue = gridSearchInput.value;
+  renderCards(searchValue);
+})
+gridModal.addEventListener("show.bs.modal", (event) => {
+  const entryID = event.relatedTarget.getAttribute("data-id");
+  const entryObject = getDataEntry(entryID);
 
-  // filter grid entries and update HTML entries
-  const searchValue = search.value;
-  displayCards(searchValue);
+  // Update the modal attributes with the object data
+  modalImg.setAttribute("src", entryObject.imageSrc);
+  modalTitle.innerText = entryObject.name;
+  modalSubtitle.innerText = entryObject.typeOfDinosaur;
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  displayCards("");
+  renderCards("");
 });
-
-
-
-  
