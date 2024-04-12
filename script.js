c0ede3e0b6da6acd666559f1ff14213386542947
Bastien Winant <bastien.winant@gmail.com>
2024-04-12 21:45:38 +0200

    dynamically update modal

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ import data from './data.json' with { type: 'json' };
 
 // DOM ELEMENTS
 const gridContainer = document.getElementById("grid-container");
-const gridSearchInput = document.getElementById("dinoName");
+const nameInput = document.getElementById("dinoName");
+const typeSelect = document.getElementById("dinoType");
+const speciesSelect = document.getElementById("dinoSpecies");
 
 const gridModal = document.getElementById("dinoModal");
 const modalImg = document.getElementById("modal-img");
 const modalTitle = document.getElementById("modal-title");
 const modalSubtitle = document.getElementById("modal-subtitle");
+const modalBody = document.getElementById("modal-body");
 
 // HELPER FUNCTIONS
 function getDataEntry(entryID) {
@@ -43,10 +46,19 @@ function renderCards(searchStr) {
 }
 
 // EVENT LISTENERS
-gridSearchInput.addEventListener("input", () => {
-  const searchValue = gridSearchInput.value;
+nameInput.addEventListener("input", () => {
+  const searchValue = nameInput.value;
   renderCards(searchValue);
-})
+});
+
+typeSelect.addEventListener("change", () => {
+  const typeValue = typeSelect.value;
+});
+
+speciesSelect.addEventListener("change", () => {
+  const speciesValue = speciesSelect.value;
+});
+
 gridModal.addEventListener("show.bs.modal", (event) => {
   const entryID = event.relatedTarget.getAttribute("data-id");
   const entryObject = getDataEntry(entryID);
@@ -55,6 +67,71 @@ gridModal.addEventListener("show.bs.modal", (event) => {
   modalImg.setAttribute("src", entryObject.imageSrc);
   modalTitle.innerText = entryObject.name;
   modalSubtitle.innerText = entryObject.typeOfDinosaur;
+
+  modalBody.innerHTML = `<div class="row text-capitalize">
+                          <div class="col border fw-bold">
+                            Name:
+                          </div>
+                          <div class="col border">
+                            ${entryObject.name}
+                          </div>
+                        </div>
+                        <div class="row text-capitalize">
+                          <div class="col border fw-bold">
+                            Type:
+                          </div>
+                          <div class="col border">
+                            ${entryObject.typeOfDinosaur}
+                          </div>
+                        </div>
+                        <div class="row text-capitalize">
+                          <div class="col border fw-bold">
+                            Species:
+                          </div>
+                          <div class="col border">
+                            ${entryObject.typeSpecies}
+                          </div>
+                        </div>
+                        <div class="row text-capitalize">
+                          <div class="col border fw-bold">
+                            Era:
+                          </div>
+                          <div class="col border">
+                            ${entryObject.whenLived}
+                          </div>
+                        </div>
+                        <div class="row text-capitalize">
+                          <div class="col border fw-bold">
+                            Found In:
+                          </div>
+                          <div class="col border">
+                            ${entryObject.foundIn}
+                          </div>
+                        </div>
+                        <div class="row text-capitalize">
+                          <div class="col border fw-bold">
+                            Diet:
+                          </div>
+                          <div class="col border">
+                            ${entryObject.diet}
+                          </div>
+                        </div>
+                        <div class="row text-capitalize">
+                          <div class="col border fw-bold">
+                            Length:
+                          </div>
+                          <div class="col border">
+                            ${entryObject.length}
+                          </div>
+                        </div>
+                        <div class="row text-capitalize">
+                          <div class="col border fw-bold">
+                            Weight:
+                          </div>
+                          <div class="col border">
+                            ${entryObject.weight}
+                          </div>
+                        </div>`;
 });
 
 document.addEventListener("DOMContentLoaded", () => {
